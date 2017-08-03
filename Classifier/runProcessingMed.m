10091f60905aa113a5860357e341f2c87f1c2170
Cuergo <andrey008@xaker.ru>
2017-08-03 16:59:16 +0300

    spectrum filtration bug fix
    1) changed input parametres
    2) added str2double functions

--- Classifier/runProcessingMed.m
+++ Classifier/runProcessingMed.m
@@ -6,19 +6,21 @@ Root = fileparts(mfilename('fullpath'));
 cd(Root);
 add_path
 
-
-
 try
-
-
 
 	%% _________________ Initialization _____________________________ %%
 	
     [file, config] = initializationMed();
         
 	%% ________________ Period Detection ____________________________ %%
-	
-	[peakLocations] = spectrumFiltration(file, config);
+	parameters = [];
+    parameters.minPeakHeight = config.config.parameters.evaluation.spectrumFiltration.Attributes.minPeakHeight;
+    parameters.timePeakDistance = config.config.parameters.evaluation.spectrumFiltration.Attributes.timePeakDistance;  
+    parameters.plotEnable = config.config.parameters.evaluation.spectrumFiltration.Attributes.plotEnable;
+    parameters.detailedPlotEnable = config.config.parameters.evaluation.spectrumFiltration.Attributes.detailedPlotEnable;
+    parameters.printPlotEnable = config.config.parameters.evaluation.spectrumFiltration.Attributes.printPlotEnable;
+	[peakLocations] = spectrumFiltration(file, parameters);
+    
 
 catch
 	% Errors checking
