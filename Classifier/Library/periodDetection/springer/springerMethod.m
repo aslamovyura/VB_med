cd5c098e3deb10cbf59451968afdc761faf8b0e3
IvanTrus <it2992i@tut.by>
2017-08-10 14:42:39 +0300

    SpringerMethod() modification
    Reconsidered handling of corner cases. Now leading and trailing RRs (and
    their corresponding s1, s2, sys, dia) whose lengthes are shorter by more
    than 25% of the mean length of all other RR segments are removed

--- Classifier/Library/periodDetection/springer/springerMethod.m
+++ Classifier/Library/periodDetection/springer/springerMethod.m
@@ -41,18 +41,35 @@ for PCGi = 1:numPCGs
 end
 
 %% Form the resulting struct which contains S1, S2, systole, diastole and RR
+% 
+% Written by Ivan Trus on 9th Aug., 2017
+% 
+% Updated on 10th Aug., 2017:
+% reconsidered handling of corner cases:
+% now leading and trailing RRs (and their corresponding s1, s2, sys, dia)
+% whose lengthes are shorter by more than "thresholdFactor" (25% by default) 
+% of the mean length of all other RR segments are removed
+%
 
-segments = struct('S1', [], 'S2', [], 'Sys', [], 'Dia' , [], 'RR', []);
-                                                
-signalLength = length(signal);
+thresholdFactor = 0.25;
 
-prevSampleState = assigned_states(1,1);
-tempSegment = signal(1,1);
+segments = struct('S1', [], 'Sys', [], 'S2', [], 'Dia' , [], 'RR', []);
+
+firstValidIndex = 1;
+
+%ignore leading null states in some signals (e.g. 107.101.1.3)
+while assigned_states(firstValidIndex,1) == 0
+   firstValidIndex = firstValidIndex+1; 
+end
+    
+prevSampleState = assigned_states(firstValidIndex,1);
+tempSegment = signal(firstValidIndex,1);
 tempRRSegment = [];
 numOfSegs = zeros(1,4);
 numOfRRs = 0;
+signalLength = length(signal);
 
-for i=2:signalLength    
+for i=firstValidIndex+1:signalLength    
     curSampleState = assigned_states(i,1);
     
     if curSampleState ~= prevSampleState
@@ -78,6 +95,7 @@ for i=2:signalLength
         if curSampleState == 1
             numOfRRs = numOfRRs + 1;
             segments.RR{numOfRRs} = tempRRSegment;
+                       
             tempRRSegment = [signal(i,1)];
         else
             tempRRSegment = [tempRRSegment signal(i,1)];
@@ -92,18 +110,98 @@ for i=2:signalLength
     
 end
 
-% add the last chunk
-numOfSegs(1, curSampleState) = numOfSegs(1, curSampleState) + 1;
-ind = numOfSegs(1, curSampleState);
-        
-switch curSampleState
 
-    case 1
-        segments.S1{ind} = tempSegment;
-    case 2
-        segments.Sys{ind} = tempSegment;
-    case 3
-        segments.S2{ind} = tempSegment;
-    case 4    
-        segments.Dia{ind} = tempSegment;
+% calculate threshold length for RR
+RRLengthes = zeros(1, numOfRRs-1);
+for i=2:numOfRRs
+    RRLengthes(1, i) = length(segments.RR{i});
+end    
+
+meanRRLength = mean(RRLengthes);
+thresholdLength = meanRRLength * thresholdFactor;
+
+% Check the very last RR's length.
+% If it satisfies the criteria then add it to the struct as well as
+% the last either s1, sys, s3 or dia segment.
+% Otherwise the aforementioned ones are not added. 
+% Also the segments which have been recently added to the struct 
+% and which correspond to the invalid RR are removed from the struct
+if abs(length(tempRRSegment)-meanRRLength) < thresholdLength
+   
+    numOfRRs = numOfRRs + 1;
+    segments.RR{numOfRRs} = tempRRSegment;
+    
+    numOfSegs(1, curSampleState) = numOfSegs(1, curSampleState) + 1;
+    ind = numOfSegs(1, curSampleState);
+
+    switch curSampleState
+
+        case 1
+            segments.S1{ind} = tempSegment;
+        case 2
+            segments.Sys{ind} = tempSegment;
+        case 3
+            segments.S2{ind} = tempSegment;
+        case 4    
+            segments.Dia{ind} = tempSegment;
+    end
+    
+else
+    switch curSampleState
+        case 2
+            numOfSegs(1,1) = numOfSegs(1,1)-1;
+            segments.S1 = segments.S1(1:numOfSegs(1,1));
+        case 3
+            numOfSegs(1,1) = numOfSegs(1,1)-1;
+            segments.S1 = segments.S1(1:numOfSegs(1,1));
+            
+            numOfSegs(1,2) = numOfSegs(1,2)-1;
+            segments.Sys = segments.Sys(1:numOfSegs(1,2));
+            
+        case 4    
+            numOfSegs(1,1) = numOfSegs(1,1)-1;
+            segments.S1 = segments.S1(1:numOfSegs(1,1));
+            
+            numOfSegs(1,2) = numOfSegs(1,2)-1;
+            segments.Sys = segments.Sys(1:numOfSegs(1,2));
+            
+            numOfSegs(1,3) = numOfSegs(1,3)-1;
+            segments.S2 = segments.S2(1:numOfSegs(1,3));
+    end 
 end
+
+% check the very first RR and its corresponding segments
+if abs(length(segments.RR{1})-meanRRLength) > thresholdLength
+    
+    segments.RR = segments.RR(2:numOfRRs);
+    numOfRRs = numOfRRs-1;
+    
+    veryFirstState = assigned_states(firstValidIndex,1);
+    
+    switch veryFirstState
+        
+        case 1 
+            segments.S1 = segments.S1(2:numOfSegs(1,1));
+            segments.Sys = segments.Sys(2:numOfSegs(1,2));
+            segments.S2 = segments.S2(2:numOfSegs(1,3));
+            segments.Dia = segments.Dia(2:numOfSegs(1,4));
+            numOfSegs = numOfSegs-1;
+            
+        case 2
+            segments.Sys = segments.Sys(2:numOfSegs(1,2));
+            segments.S2 = segments.S2(2:numOfSegs(1,3));
+            segments.Dia = segments.Dia(2:numOfSegs(1,4));
+            numOfSegs(1,2:4) = numOfSegs(1,2:4)-1;
+        case 3
+            segments.S2 = segments.S2(2:numOfSegs(1,3));
+            segments.Dia = segments.Dia(2:numOfSegs(1,4));
+            numOfSegs(1,3:4) = numOfSegs(1,3:4)-1;     
+        case 4    
+            segments.Dia = segments.Dia(2:numOfSegs(1,4));
+            numOfSegs(1,4) = numOfSegs(1,4)-1;     
+    end 
+       
+end
+
+
+
